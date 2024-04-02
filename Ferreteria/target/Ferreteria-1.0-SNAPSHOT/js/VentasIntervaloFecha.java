661cdb7bc2e1f66b7d2135a37aa3a8d777091d22
JesusMayora <164205269+JesusMayora@users.noreply.github.com>
2024-04-02 11:09:50 -0600

    subir a ft
    crud y consultas

--- Ferreteria/target/Ferreteria-1.0-SNAPSHOT/js/VentasIntervaloFecha.java
+++ Ferreteria/target/Ferreteria-1.0-SNAPSHOT/js/VentasIntervaloFecha.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.ues.models.dtos;
 
-/**
- *
- * @author herna
- */
+
 public class VentasIntervaloFecha {
     private String nombreEmpleado;
     private int totalDeVentas;
