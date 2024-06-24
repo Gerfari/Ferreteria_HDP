73a3cac2430449066c10aed743196af95c5c26dc
KevSant10 <santoskevin439@gmail.com>
2024-06-23 22:45:32 -0600

    Menu Terminado

--- Ferreteria/target/Ferreteria-1.0-SNAPSHOT/js/prod_vendidos.js
+++ Ferreteria/target/Ferreteria-1.0-SNAPSHOT/js/prod_vendidos.js
@@ -1,3 +1,4 @@
+verificarSesion();
 $(function(){
     console.log("Entro al javascript");
     cargarTablaProductos();
@@ -34,6 +35,15 @@ function cargarTablaProductos(estado=1){
     }).always(function(){
         
     });
+   
+ 
     
-    
+}
+   function verificarSesion(){
+    let empleado = JSON.parse(localStorage.getItem('empleado'));
+    if(empleado == null) {
+        console.log("Deberia mostrar mensaje de error");
+        window.top.location.href = '../Utilidades/RestringirAcceso.jsp';
+    }
+    console.log("Paso el if de verificarSesion");
 }
